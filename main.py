75df970ce7fd26d820953cecfef0cd66d5705560
zar4za <zar4z4@yandex.ru>
2023-10-04 10:36:10 +0300

    feat: product categories combinations query

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+from pyspark.sql import SparkSession
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+spark = SparkSession.builder.getOrCreate()
+expected_df = spark.createDataFrame([
+    {"product": "Product 1", "category": "Category 1"},
+    {"product": "Product 1", "category": "Category 2"},
+    {"product": "Product 2", "category": None},
+    {"product": "Product 3", "category": "Category 3"},
+    {"product": "Product 4", "category": "Category 4"}
+])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Storage:
+    def __init__(self):
+        self.__products = spark.createDataFrame([
+            {"id": 1, "name": "Product 1", "description": "test"},
+            {"id": 2, "name": "Product 2", "description": "test"},
+            {"id": 3, "name": "Product 3", "description": "test"},
+            {"id": 4, "name": "Product 4", "description": "test"},
+        ])
+
+        self.__categories = spark.createDataFrame([
+            {"id": 1, "name": "Category 1", "description": "test"},
+            {"id": 2, "name": "Category 2", "description": "test"},
+            {"id": 3, "name": "Category 3", "description": "test"},
+            {"id": 4, "name": "Category 4", "description": "test"},
+        ])
+
+        self.__product_categories = spark.createDataFrame([
+            {"product_id": 1, "category_id": 1},
+            {"product_id": 1, "category_id": 2},
+            {"product_id": 3, "category_id": 3},
+            {"product_id": 4, "category_id": 4},
+        ])
+
+    def get_product_category_df(self):
+        combs = self.__products.join(
+            self.__product_categories,
+            self.__products.id == self.__product_categories.product_id,
+            how="full").join(
+                self.__categories,
+                self.__product_categories.category_id == self.__categories.id,
+                how="full"
+            )
+
+        return combs.select(self.__products.name.alias("product"), self.__categories.name.alias("category"))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+storage = Storage()
+df = storage.get_product_category_df()
+df.show()
